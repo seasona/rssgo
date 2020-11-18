a2e30ed07b574e273d5065e17fdf981a9eceffe6
Jie Ji <396438446@qq.com>
2020-11-18 17:24:57 +0800

    fix: can't use placeholder ? in place is not attribute

--- internal/db.go
+++ internal/db.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -24,18 +25,20 @@ func (d *DB) Init(c *Controller, dbFile string) {
 }
 
 func (d *DB) CreateTables(db *sql.DB, rss *RSS) {
+	// the rss title may contain space 
 	for _, tu := range rss.titleURLs {
-		_, err := d.db.Exec(`
-         create table if not exists ?(
-			id integer not null primary key,
-			feed text,
-			title text,
-			content text,
-			link text,
-			read bool,
-			display_name string,
-			deleted bool,
-			published DATETIME);`, tu.Title)
+		ct := fmt.Sprintf(`create table if not exists %v(
+				id integer not null primary key,
+				feed text,
+				title text,
+				content text,
+				link text,
+				read bool,
+				display_name string,
+				deleted bool,
+				published DATETIME);`, tu.Title)
+
+		_, err := d.db.Exec(ct)
 
 		if err != nil {
 			log.Panicf("Can't create database %v because: %v", tu.Title, err)
